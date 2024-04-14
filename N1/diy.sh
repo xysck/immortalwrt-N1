a33f6867568d3bb3b85c90fb3dcafc4d2514005a
Chris <152150297+innmonkey@users.noreply.github.com>
2024-04-14 19:17:47 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -39,7 +39,7 @@ CONFIG_PACKAGE_luci-app-linkease=y
 sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
