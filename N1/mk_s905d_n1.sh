3c7c7f375829151c556f004809c5817c2c975f29
Chris <152150297+innmonkey@users.noreply.github.com>
2024-04-15 14:46:53 +0800

    Update mk_s905d_n1.sh

--- N1/mk_s905d_n1.sh
+++ N1/mk_s905d_n1.sh
@@ -172,8 +172,10 @@ adjust_openssl_config
 adjust_getty_config
 adjust_samba_config
 adjust_openssh_config
+adjust_openclash_config
 use_xrayplug_replace_v2rayplug
 create_fstab_config
+adjust_turboacc_config
 adjust_mosdns_config
 patch_admin_status_index_html
 adjust_kernel_env
